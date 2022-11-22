2ad8f83b30eccfcba312ee23606e960805e4d71f
Renier Reichert <r.a.reichert@outlook.com>
2022-11-22 10:15:36 +0100

    Initial commit

---
+++ TodoListProject/TodoListProject/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TodoListProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<Todo> todoList = new List<Todo>();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var t = new Todo((i % 2 == 0), DateTime.Now, $"This is task number {i}");
+                todoList.Add(t);
+            }
+        }
+
+    }
+
+    public class Todo
+    {
+        public bool done { get; set; }
+        public DateTime dueDate { get; set; }
+        public string? description { get; set; }
+
+        public Todo(bool done, DateTime dueDate, string? description)
+        {
+            this.done = done;
+            this.dueDate = dueDate;
+            this.description = description;
+        }
+    }
+}
